a53fafd5eccf1af9f96686f6329b4ca6e215b163
Juan Antonio Gil Carrera <jagilcarrera@gmail.com>
2020-10-04 01:32:56 +0200

    Creado el paquete "gui" y hola mundo

---
+++ gui/JVentana.java
@@ -0,0 +1,12 @@
+package despaching.gui;
+
+/**Esta clase contiene el main, y sera la que contendra todos los paneles de todos los usuarios y 
+ * la que se encargara de pasar entre ellas
+ */
+public class JVentana{
+
+    /**Metodo main, por ahora solo muestra un hola mundo por terminal*/
+    public static void main(String[] args){
+        System.out.println("Hola mundo");
+    }
+}
