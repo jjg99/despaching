c5c4fd7551c4deeefd5e911a1122d00ef3ec3146
Juan Antonio Gil Carrera <jagilcarrera@gmail.com>
2020-10-04 02:25:53 +0200

    Cambio de estructura a una usada en java

--- src/gui/JVentanaApp.java
+++ src/gui/JVentanaApp.java
@@ -1,14 +1,13 @@
-package despaching.gui;
+package gui;
 
-import despaching.test;
-
-/**Esta clase contiene el main, y sera la que contendra todos los paneles de todos los usuarios y 
- * la que se encargara de pasar entre ellas
+/**
+ * Esta clase contiene el main, y sera la que contendra todos los paneles de
+ * todos los usuarios y la que se encargara de pasar entre ellas
  */
-public class JVentana{
+public class JVentanaApp{
 
     /**Metodo main, por ahora solo muestra un hola mundo por terminal*/
     public static void main(String[] args){
-        prueba.print();
+        System.out.println("Hola mundo");
     }
 }
