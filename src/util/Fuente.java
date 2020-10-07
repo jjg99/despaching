a42b0f2968becae4d619d85e3e2ec29b823ae138
J0nan <jagilcarrera@gmail.com>
2020-10-07 22:33:41 +0200

    Clase fuente actualizada

--- src/util/Fuente.java
+++ src/util/Fuente.java
@@ -9,26 +9,28 @@ import javax.swing.JComponent;
  * mantener consistencia a lo largo del programa.
  */
 public class Fuente {
-    /**Fija el tipo de letra a <code>Font.SANS_SERIF</code>*/
+    /**Fija el tipo de letra a <code>SANS_SERIF</code>*/
     private final static String FUENTE = Font.SANS_SERIF;
 
+    /**Fija el tamaño de la letra de los titulos*/
     private final static int TAM_TITULO = 30;
 
+    /**Fija el tamaño de la letra del cuerpo*/
     private final static int TAM_CUERPO = 20;
 
-    public static void SetFuente(JComponent comp){
+    public static void setFuente(JComponent comp){
         comp.setFont(new Font(FUENTE, Font.PLAIN, TAM_CUERPO));
     }
 
-    public static void SetFuenteNegrita(JComponent comp){
+    public static void setFuenteNegrita(JComponent comp){
         comp.setFont(new Font(FUENTE, Font.BOLD, TAM_CUERPO));
     }
 
-    public static void SetFuenteTitulo(JComponent comp){
+    public static void setFuenteTitulo(JComponent comp){
         comp.setFont(new Font(FUENTE, Font.PLAIN, TAM_TITULO));
     }
 
-    public static void SetFuenteTituloNegrita(JComponent comp){
+    public static void setFuenteTituloNegrita(JComponent comp){
         comp.setFont(new Font(FUENTE, Font.BOLD, TAM_TITULO));
     }
 }
