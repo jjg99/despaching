a5cd91cdd54d7a55381d2adf54fd3d0bbeabf606
J0nan <jagilcarrera@gmail.com>
2020-11-15 03:57:10 +0100

    Agregado el icono a la app

--- src/gui/JVentana.java
+++ src/gui/JVentana.java
@@ -1,59 +1,74 @@
 package gui;
 
 import java.awt.BorderLayout;
+import java.net.MalformedURLException;
+import java.net.URL;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 import paneluser.PnlInicio;
 import util.Colores;
+
 /**
  * Esta clase contiene el main, y sera la que contendra todos los paneles de
  * todos los usuarios y la que se encargara de pasar entre ellas.
  */
-public class JVentana extends JFrame{
-    /**String con el nombre que tendra la ventanga generada */
+public class JVentana extends JFrame {
+    /** String con el nombre que tendra la ventanga generada */
     private final String Titulo = new String("Despaching");
 
-    /** Panel que contendra todos los componentes y ocupara la totalidad de la <code>JVentana</code> */
+    /**
+     * Panel que contendra todos los componentes y ocupara la totalidad de la
+     * <code>JVentana</code>
+     */
     private static JPanel pnlVentana = new JPanel();
 
     /**
      * Metodo main crea una instancia de JVentanaApp
      */
-    public static void main(String[] args){
+    public static void main(String[] args) {
         new JVentana();
     }
-    
+
     /**
-     * Crea una nueva JVentanaApp, llamando al metodo {@link initComponents} para instanciar el 
-     * panel inicial e {@link initFrame} para configurar y mostrar la ventana.
+     * Crea una nueva JVentanaApp, llamando al metodo {@link initComponents} para
+     * instanciar el panel inicial e {@link initFrame} para configurar y mostrar la
+     * ventana.
      */
-    public JVentana(){
+    public JVentana() {
         initComponents();
         initFrame();
-        this.requestFocusInWindow();    //Hacemos focus en la JVentana para que no empiece en el Textfield de Usuario
+        this.requestFocusInWindow(); // Hacemos focus en la JVentana para que no empiece en el Textfield de Usuario
     }
 
     /**
-     * Metodo donde se inicializan los componentes iniciales de la aplicacion, es decir, 
-     * el {@link PnlInicio}
+     * Metodo donde se inicializan los componentes iniciales de la aplicacion, es
+     * decir, el {@link PnlInicio}
      */
-    private void initComponents(){
-        pnlVentana.setLayout(new BorderLayout());     //Fijamos el tipo de Layout
-        Colores.setBGColor(pnlVentana);    //Fijamos el color del fondo
-        pnlVentana.add(PnlInicio.PnlInicio,BorderLayout.CENTER);      //Añadimos el panel de inicio
+    private void initComponents() {
+        pnlVentana.setLayout(new BorderLayout()); // Fijamos el tipo de Layout
+        Colores.setBGColor(pnlVentana); // Fijamos el color del fondo
+        pnlVentana.add(PnlInicio.PnlInicio, BorderLayout.CENTER); // Añadimos el panel de inicio
 
-        this.setLayout(new BorderLayout());     //Fijamos el tipo de Layout
-        this.add(pnlVentana,BorderLayout.CENTER);      //Añadimos el panel de la ventana
+        this.setLayout(new BorderLayout()); // Fijamos el tipo de Layout
+        this.add(pnlVentana, BorderLayout.CENTER); // Añadimos el panel de la ventana
     }
 
     /**
-     * Metodo en el cual se configura el tamaño, se fija un {@linkplain Titulo} a la ventana,
-     * se activa la visibilidad, se fija la accion al cerrarse, se fija la localizacion y 
-     * se hace que no se pueda cambiar el tamaño de la ventana.
+     * Metodo en el cual se configura el tamaño, se fija un {@linkplain Titulo} a la
+     * ventana, se activa la visibilidad, se fija la accion al cerrarse, se fija la
+     * localizacion y se hace que no se pueda cambiar el tamaño de la ventana.
      */
-    private void initFrame(){
+    private void initFrame() {
+        try {
+            this.setIconImage(
+                    (new ImageIcon(new URL("https://img.icons8.com/dusk/32/000000/add-user-group-man-woman.png")))
+                            .getImage());
+        } catch (MalformedURLException e){
+            e.printStackTrace();
+        }
         this.setSize(600,600);  //Fijamos el tamaño de la ventana a 600x600
         this.setTitle(Titulo);  //Ponemos el titulo a la ventana
         this.setVisible(true);  //Hacemos la ventana visible
