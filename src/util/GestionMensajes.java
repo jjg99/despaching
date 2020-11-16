6d9d0cb661f4ee5bbc900a4fac6d6942d8693f10
J0nan <jagilcarrera@gmail.com>
2020-11-16 03:57:21 +0100

    Añadido mensaje pregunta generica de dos opciones

--- src/util/GestionMensajes.java
+++ src/util/GestionMensajes.java
@@ -1,50 +1,75 @@
 package util;
 
+import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
 
 import java.io.StringWriter;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.awt.HeadlessException;
 import java.io.PrintWriter;
 
 /**
-* Esta clase se encarga de la gestión de mensajes de errores, emitiendolos una vez
-* llamados sus metodos o interpretando el tipo de error presentado en el programa.
-*/
+ * Esta clase se encarga de la gestión de mensajes de errores, emitiendolos una
+ * vez llamados sus metodos o interpretando el tipo de error presentado en el
+ * programa.
+ */
 public class GestionMensajes {
-  
+
     /**
-     * Metodo que se encarga de emitir un mensaje cuando el usuario
-     * y/o contraseña introducidos son erroneos.
+     * Metodo que se encarga de emitir un mensaje cuando el usuario y/o contraseña
+     * introducidos son erroneos.
      */
-    public static void msgErrorInicioSesion()
-    {
-        JOptionPane.showMessageDialog(null, "Usuario y/o contrasena incorrectos", "Error Inicio Sesión", JOptionPane.WARNING_MESSAGE);
+    public static void msgErrorInicioSesion() {
+        JOptionPane.showMessageDialog(null, "Usuario y/o contrasena incorrectos", "Error Inicio Sesión",
+                JOptionPane.WARNING_MESSAGE);
     }
 
     /**
-     * Metodo que emite un mensaje cuando un alumno intenta realizar una cita
-     * cuando no hay disponibilidad
+     * Metodo que emite un mensaje cuando un alumno intenta realizar una cita cuando
+     * no hay disponibilidad
      */
-    public static void msgErrorDisponibilidad()
-    {
-        JOptionPane.showMessageDialog(null, "No existe disponibilidad para reservar con este profesor", "Error", JOptionPane.INFORMATION_MESSAGE);
+    public static void msgErrorDisponibilidad() {
+        JOptionPane.showMessageDialog(null, "No existe disponibilidad para reservar con este profesor", "Error",
+                JOptionPane.INFORMATION_MESSAGE);
     }
 
     /**
-     * Metodo que emite un mensaje de error cuando no se ha podido conectar
-     * con la base de datos
+     * Metodo que emite un mensaje de error cuando no se ha podido conectar con la
+     * base de datos
      */
-    public static void msgErrorBD()
-    {
-        JOptionPane.showMessageDialog(null, "No se ha podido conectar con la base de datos, compruebe su conexion a Internet", "Error", JOptionPane.WARNING_MESSAGE);
+    public static void msgErrorBD() {
+        JOptionPane.showMessageDialog(null,
+                "No se ha podido conectar con la base de datos, compruebe su conexion a Internet", "Error",
+                JOptionPane.WARNING_MESSAGE);
     }
 
-    public static void msgErrorGenerico(Exception e)
-    {
+    public static void msgErrorGenerico(Exception e) {
         StringWriter sw = new StringWriter();
         PrintWriter pw = new PrintWriter(sw);
         e.printStackTrace(pw);
         String mensaje_error = sw.toString();
 
         JOptionPane.showMessageDialog(null, mensaje_error, "Error", JOptionPane.WARNING_MESSAGE);
+    }
+
+    /**
+     * Metodo que muestra realiza una pregunta y da al usuario dos opciones para responder.
+     * @param opciones es de tipo <code>Object[]</code> y debera tener dos objetos, estos seran los que se mostrara al usuario
+     *                 para elegir, es importante que el que segundo sea la opcion que se quiere por defecto.
+     * @param mensaje es el texto que se el mostrara al usuario en la ventana.
+     * @param titulo sera el titulo de la ventana emerjente
+     * @return la posicion del array <code>opciones</code> que ha sido selecionado.
+     */
+    public static int msg2OpcionesGenerico(Object[] opciones, String mensaje, String titulo) {
+        try {
+            return JOptionPane.showOptionDialog(null, mensaje, titulo, JOptionPane.DEFAULT_OPTION,
+                    JOptionPane.QUESTION_MESSAGE,
+                    new ImageIcon(new URL("https://img.icons8.com/dusk/64/000000/question-mark.png")), opciones, opciones[1]);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return 1;
+        }
+        
     }
 }
