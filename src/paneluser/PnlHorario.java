2c467044edda624daec9627f057cfde252e262d6
felixgims <201708084@alu.comillas.edu>
2020-11-18 10:08:03 +0100

    el horario los S y D se pintan de azul

--- src/paneluser/PnlHorario.java
+++ src/paneluser/PnlHorario.java
@@ -55,66 +55,70 @@ public class PnlHorario extends JPanel{
         String horario= Fachada.getHorario(usuario.getId()); // se consigue el horario de la base de datos
         String[] clases = horario.split(";");  // se separa en dias de la semana
         int dia = Fecha.getDiaSemana() -2; // variable que contiene el numero del dia que es
-
-        String[] horas = clases[dia].split(","); // se separa las clases del dia
-        int horafinant = 0; // varible que contiene la hora anterior a la clase con la que se comprara si hay solaapamiento
-        int minutosfinant = 0; // variable que contiene los minutos anteriores a la clase con la que se compara si hay solapamientos
-        for (int i=1; i< horas.length ;i++){ // bucle que rrecore las clases del dia
-            int horaini = Integer.valueOf(String.valueOf(horas[i].charAt(0)) + String.valueOf(horas[i].charAt(1))); // variable que almacena la hora de inicio de la clase
-            int minutosini = Integer.valueOf(String.valueOf(horas[i].charAt(3)) + String.valueOf(horas[i].charAt(4))); // variable que almacena los minutos de inicio de la clase
-            int horafin = Integer.valueOf(String.valueOf(horas[i].charAt(6)) + String.valueOf(horas[i].charAt(7))); // variable que almacena la hora de fin de la clase
-            int minutosfin = Integer.valueOf(String.valueOf(horas[i].charAt(9)) + String.valueOf(horas[i].charAt(10))); // variable que almacena los minutos de fin de la clase
-            if (minutosini == 0) // cambia el boton al color que le corresponde
-                if (horaini-7 > 7 )
-                    Colores.setBGRojo(btnHoras[horaini -15][1]);
-                else{
-                    Colores.setBGRojo(btnHoras[horaini -8][0]);
-                }
-            else{
-                if (horaini == horafinant && (minutosini == minutosfinant || minutosini == minutosfinant - 10))
-                    if (horaini-7 > 7 ){
+        if (dia == -1 || dia == 6){ // los sabados y domingos pintan en gris pues no hay clases
+            for (int i=0; i<7; i++)
+                for (int j=0; j<2;j++)
+                    Colores.setBGAzul(btnHoras[i][j]);
+        } 
+        else {
+            String[] horas = clases[dia].split(","); // se separa las clases del dia
+            int horafinant = 0; // varible que contiene la hora anterior a la clase con la que se comprara si hay solaapamiento
+            int minutosfinant = 0; // variable que contiene los minutos anteriores a la clase con la que se compara si hay solapamientos
+            for (int i=1; i< horas.length ;i++){ // bucle que rrecore las clases del dia
+                int horaini = Integer.valueOf(String.valueOf(horas[i].charAt(0)) + String.valueOf(horas[i].charAt(1))); // variable que almacena la hora de inicio de la clase
+                int minutosini = Integer.valueOf(String.valueOf(horas[i].charAt(3)) + String.valueOf(horas[i].charAt(4))); // variable que almacena los minutos de inicio de la clase
+                int horafin = Integer.valueOf(String.valueOf(horas[i].charAt(6)) + String.valueOf(horas[i].charAt(7))); // variable que almacena la hora de fin de la clase
+                int minutosfin = Integer.valueOf(String.valueOf(horas[i].charAt(9)) + String.valueOf(horas[i].charAt(10))); // variable que almacena los minutos de fin de la clase
+                if (minutosini == 0) // cambia el boton al color que le corresponde
+                    if (horaini-7 > 7 )
                         Colores.setBGRojo(btnHoras[horaini -15][1]);
-                        if (horafin-7 > 7 ){
-                            Colores.setBGAmarillo(btnHoras[horafin -15][1]);
-                        }
-                        else{
-                            Colores.setBGAmarillo(btnHoras[horafin -8][0]);
-                        }
-                    }
                     else{
                         Colores.setBGRojo(btnHoras[horaini -8][0]);
-                        if (horafin-7 > 7 ){
-                            Colores.setBGAmarillo(btnHoras[horafin -15][1]);
-                        }
-                        else{
-                            Colores.setBGAmarillo(btnHoras[horafin -8][0]);
-                        }
                     }
                 else{
-                    if (horaini-7 > 7 ){
-
-                        Colores.setBGAmarillo(btnHoras[horaini -15][1]);
-                        if (horafin-7 > 7 ){
-                            Colores.setBGAmarillo(btnHoras[horafin -15][1]);
+                    if (horaini == horafinant && (minutosini == minutosfinant || minutosini == minutosfinant - 10))
+                        if (horaini-7 > 7 ){
+                            Colores.setBGRojo(btnHoras[horaini -15][1]);
+                            if (horafin-7 > 7 ){
+                                Colores.setBGAmarillo(btnHoras[horafin -15][1]);
+                            }
+                            else{
+                                Colores.setBGAmarillo(btnHoras[horafin -8][0]);
+                            }
                         }
                         else{
-                            Colores.setBGAmarillo(btnHoras[horafin -8][0]);
+                            Colores.setBGRojo(btnHoras[horaini -8][0]);
+                            if (horafin-7 > 7 ){
+                                Colores.setBGAmarillo(btnHoras[horafin -15][1]);
+                            }
+                            else{
+                                Colores.setBGAmarillo(btnHoras[horafin -8][0]);
+                            }
                         }
-                    }
                     else{
-                        Colores.setBGAmarillo(btnHoras[horaini -8][0]);
-                        if (horafin-7 > 7 ){
-                            Colores.setBGAmarillo(btnHoras[horafin -15][1]);
+                        if (horaini-7 > 7 ){
+                            Colores.setBGAmarillo(btnHoras[horaini -15][1]);
+                            if (horafin-7 > 7 ){
+                                Colores.setBGAmarillo(btnHoras[horafin -15][1]);
+                            }
+                            else{
+                                Colores.setBGAmarillo(btnHoras[horafin -8][0]);
+                            }
                         }
                         else{
-                            Colores.setBGAmarillo(btnHoras[horafin -8][0]);
+                            Colores.setBGAmarillo(btnHoras[horaini -8][0]);
+                            if (horafin-7 > 7 ){
+                                Colores.setBGAmarillo(btnHoras[horafin -15][1]);
+                            }
+                            else{
+                                Colores.setBGAmarillo(btnHoras[horafin -8][0]);
+                            }
                         }
                     }
                 }
+                horafinant = horafin;
+                minutosfinant = minutosfin; 
             }
-            horafinant = horafin;
-            minutosfinant = minutosfin; 
-        }
-
+        } 
     }
 }
