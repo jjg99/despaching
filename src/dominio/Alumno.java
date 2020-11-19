3720aef31f21715ad1cf92cd3322a9607bc820f1
rulopelon <48516780+rulopelon@users.noreply.github.com>
2020-11-19 10:42:17 +0100

    cambio visibilidad

--- src/dominio/Alumno.java
+++ src/dominio/Alumno.java
@@ -10,22 +10,22 @@ public class Alumno extends Usuario  {
     private ArrayList<String> listaProfesores = new ArrayList<String>();        // arrayList que va a contener todos los porfesores del alumno
     private ArrayList<String> listaColas = new ArrayList<String>();        // arrayList que contiene los nombres de 
 
-    public Alumno (String correo,String password){
+    private Alumno (String correo,String password){
         super(correo);  // se inicia el objeto, instanciando al objeto superior     
     }
 
     /** constructor que inicia todos los atributos de un usuario */
-    public Alumno (String correo, String nombre, String apellido, String id){
+    private Alumno (String correo, String nombre, String apellido, String id){
         super(correo,nombre,apellido,id);     // se inicia el objeto, instanciando al objeto superior
     }
 
     /**Metodo para conseguir una lista de todos los profesores del alumno, crea un {@link ArrayList} de @Profesor */
-    public void getProfesores(){ 
+    private void getProfesores(){ 
         listaProfesores = Fachada.getProfesoresAlumno(super.getId());
     }
 
     /**Utiliza el metodo de la clase @link Fachada, para conectarse a la base de datos y pedir las colas en las que se encuentra el alumno */
-    public void getColas(){
+    private void getColas(){
         listaColas = GestorCola.getColasAlumno(super.getId());      // le pide al gestor de colas que le devuelva la lista de colas a la que se encuentra apuntado el alumno
     }
 }
