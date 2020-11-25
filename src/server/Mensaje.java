c94c659e5f1e38f210155f093691623261816743
rulopelon <48516780+rulopelon@users.noreply.github.com>
2020-11-25 23:52:26 +0100

    Clase mensaje

---
+++ src/server/Mensaje.java
@@ -0,0 +1,37 @@
+package server;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Mensaje implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String context;
+	private Map<String, Object> contenido;
+	
+	
+	public Mensaje () {
+		context=new String();
+		contenido=new HashMap<String, Object>();	// en este campo se guardan todos los objetos que sea ncesario enviar por medio del socket, puede quedar vacio
+		
+	}
+
+	public String getContext() {
+		return context;
+	}
+
+	public void setContext(String context) {
+		this.context = context;
+	}
+
+	public Map<String, Object> getContenido() {
+		return contenido;
+	}
+
+	public void setContenido(Map<String, Object> session) {
+		this.contenido = session;
+	}	
+}
